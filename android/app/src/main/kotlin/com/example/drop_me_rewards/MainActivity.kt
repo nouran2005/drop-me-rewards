8a9d38bd7ae8dd554a905fa4e13ea3ae21e1a2bb
Nouran Samer <nouransamer5@gmail.com>
2026-09-21 12:42:12 +0300

    initialize project structure

---
+++ android/app/src/main/kotlin/com/example/drop_me_rewards/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drop_me_rewards
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'core/constants/app_constants.dart';
+void main() {
+
+  runApp(
+    MaterialApp(
+        title: AppConstants.appName,
+        debugShowCheckedModeBanner: false,
+        home: const Placeholder(),
+      ),
+  );
+}
